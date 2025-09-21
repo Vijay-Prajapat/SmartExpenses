f17223cf83ec64632511e3b8b65baf04eeb34340
Vijay Prajapat <sanskarprajapt2002@gmail.com>
2025-09-21 20:46:37 +0530

    Update to cors

--- server.js
+++ server.js
@@ -10,11 +10,20 @@ const expenseRoutes = require("./Routes/expenseRoutes");
 app.use(express.json());
 app.use(cookieParser());
 
+const allowedOrigins = ["http://localhost:5173", "https://smart-expenses123.netlify.app"];
+
 app.use(cors({
-  origin: ["http://localhost:5173", "https://smart-expenses123.netlify.app"],
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // allow non-browser requests like Postman
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"], // allow needed methods
-  allowedHeaders: ["Content-Type", "Authorization"] // allow JWT/auth headers
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 
